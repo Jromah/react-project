c34f06e2de65198e4c163e83871d04b53226bfd4
Jean Rodriguez <91.jroma@gmail.com>
2024-10-13 21:50:50 -0300

    creando el contexto

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,20 @@
-import { Box, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import myImage from '../../assets/Cart-icon.png';
+import { CartContext } from "../../context";
 
 export const CartWidget = () => {
+
+    const { cartState } = useContext(CartContext);
     return (
-        <Box>
+        <Flex
+            alignItems={"center"}
+            height={"100%"}
+            justifyContent={"space-between"}
+            width={"60px"}>
             <img src={myImage} alt="logo-carrito-de-compras" />
-            <Text>2</Text>
-        </Box>
+            <Text fontSize={"1.5rem"}>{cartState}</Text>
+        </Flex>
 
     )
 };
