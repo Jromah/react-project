e34101f6ef72927e583cb0c66d014f0d0d516d6f
Jean Rodriguez <91.jroma@gmail.com>
2024-10-19 00:34:57 -0300

    cheackout y fin entrega?

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import myImage from '../../assets/Cart-icon.png';
 import { CartContext } from "../../context";
+import { Link } from "react-router-dom";
 
 export const CartWidget = () => {
 
@@ -17,7 +18,7 @@ export const CartWidget = () => {
             justifyContent={"space-between"}
             width={"60px"}>
             <img src={myImage} alt="logo-carrito-de-compras" />
-            <Text fontSize={"1.5rem"}>{qtyTotalItems}</Text>
+            <Link to="/checkout"><Text fontSize={"1.5rem"}>{qtyTotalItems}</Text></Link>
         </Flex>
 
     )
--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, Item, Category } from '../pages';
+import { Home, Item, Category, Checkout } from '../pages';
 import { Navbar } from '../components';
 
 export const MainRouter = () => {
@@ -11,6 +11,7 @@ export const MainRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/item/:id" element={<Item />} />
         <Route path="/category/:categoryId" element={<Category />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
